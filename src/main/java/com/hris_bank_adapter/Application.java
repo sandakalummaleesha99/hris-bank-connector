45fb900927953d6a69d24c6b5fb0057d5702876f
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-04-09 17:45:23 +0530

    changes

---
+++ src/main/java/com/hris_bank_adapter/Application.java
@@ -0,0 +1,13 @@
+package com.hris_bank_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
